2ffa05d82dce5c1344b6e0665987a461024c50b8
TeTeBerry <250714751@qq.com>
2020-05-02 16:42:58 +0800

    fetch data

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Head from "next/head";
 import Layout from "../../components/layout";
+import { Button } from "antd";
 
 export default function FirstPost() {
   return (
@@ -11,7 +12,7 @@ export default function FirstPost() {
       <h1>First Post</h1>
       <h2>
         <Link href="/">
-          <a className="home">Back to home</a>
+          <a></a>
         </Link>
       </h2>
     </Layout>
